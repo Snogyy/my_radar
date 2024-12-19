c7341e8f71356ea74928dee08c2e273095390969
Jules Nourdin <jules.nourdin@epitech.eu>
2024-12-19 15:30:48 +0100

    feat(all): adding collision between planes/planes and planes/towers

--- get_info.c
+++ get_info.c
@@ -12,7 +12,7 @@ void line_to_infos(char *line, int *infos)
     int index = 0;
     int value = 0;
 
-    for (int i = 0; line[i] != '\n'; i++) {
+    for (int i = 0; i != my_strlen(line); i++) {
         if (line[i] >= '0' && line[i] <= '9')
             value = (value * 10) + (line[i] - 48);
         if ((line[i] == ' ' && value != 0)) {
@@ -24,46 +24,46 @@ void line_to_infos(char *line, int *infos)
     infos[index] = value;
 }
 
-void get_info_plane(sprite_t *sprite, char *path)
+int get_info_plane(sprite_t *sprite, char *line, int index)
 {
-    FILE *file = fopen(path, "r");
     int *infos = malloc(sizeof(int) * 6);
-    char *line = NULL;
-    size_t size = 0;
-    int idx = 0;
 
-    for (int i = 0; getline(&line, &size, file) != -1; i++) {
-        if (line[0] == 'A') {
-            line_to_infos(line, infos);
-            sprite->plane[idx].dep_pos = (sfVector2f){infos[0], infos[1]};
-            sprite->plane[idx].arr_pos = (sfVector2f){infos[2], infos[3]};
-            sprite->plane[idx].speed = infos[4];
-            sprite->plane[idx].delay = infos[5];
-            idx++;
-        }
-    }
+    line_to_infos(line, infos);
+    sprite->plane[index].dep_pos = (sfVector2f){infos[0], infos[1]};
+    sprite->plane[index].arr_pos = (sfVector2f){infos[2], infos[3]};
+    sprite->plane[index].speed = infos[4];
+    sprite->plane[index].delay = infos[5];
+    index++;
     free(infos);
-    free(line);
-    fclose(file);
+    return index;
 }
 
-void get_info_tower(sprite_t *sprite, char *path)
+int get_info_tower(sprite_t *sprite, char *line, int index)
+{
+    int *infos = malloc(sizeof(int) * 3);
+
+    line_to_infos(line, infos);
+    sprite->tower[index].pos = (sfVector2f){infos[0], infos[1]};
+    sprite->tower[index].radius = infos[2];
+    index++;
+    free(infos);
+    return index;
+}
+
+void get_info(sprite_t *sprite, char *path, int is_plane)
 {
     FILE *file = fopen(path, "r");
-    int *infos = malloc(sizeof(int) * 3);
     char *line = NULL;
     size_t size = 0;
-    int idx = 0;
+    int idx_plane = 0;
+    int idx_tower = 0;
 
     for (int i = 0; getline(&line, &size, file) != -1; i++) {
-        if (line[0] == 'T') {
-            line_to_infos(line, infos);
-            sprite->tower[idx].pos = (sfVector2f){infos[0], infos[1]};
-            sprite->tower[idx].radius = infos[2];
-            idx++;
-        }
+        if (line[0] == 'A' && is_plane == 1)
+            idx_plane = get_info_plane(sprite, line, idx_plane);
+        if (line[0] == 'T' && is_plane == 0)
+            idx_tower = get_info_tower(sprite, line, idx_tower);
     }
-    free(infos);
     free(line);
     fclose(file);
 }
@@ -71,14 +71,17 @@ void get_info_tower(sprite_t *sprite, char *path)
 int get_nbelemt(char *path, int is_plane)
 {
     FILE *file = fopen(path, "r");
-    char type;
+    char *line = NULL;
+    size_t size = 0;
     int counter = 0;
 
-    while (fscanf(file, "%c", &type) != -1) {
-        if (type == 'A' && is_plane == 1)
+    for (int i = 0; getline(&line, &size, file) != -1; i++) {
+        if (line[0] == 'A' && is_plane == 1)
             counter++;
-        if (type == 'T' && is_plane == 0)
+        if (line[0] == 'T' && is_plane == 0)
             counter++;
     }
+    free(line);
+    fclose(file);
     return counter;
 }
